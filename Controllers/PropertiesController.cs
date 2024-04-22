d2dca0285712f260b7ef7b72d03e53926578f14f
Yasser Bousrih <Bousrihyasser@gmail.com>
2024-04-22 04:25:45 -0400

    search works Happy man

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -16,52 +16,21 @@ namespace PropertyRentalManagementWebSite.Controllers
     public class PropertiesController : Controller
     {
         private PropertyRentalManagementWebSiteEntities db = new PropertyRentalManagementWebSiteEntities();
-
-
-        // GET: Properties
-        public ActionResult Index(string address)
+        // GET: Properties/CreateWithManager
+        public ActionResult CreateWithManager()
         {
-            var properties = from p in db.Properties
-                             select p;
+            // Create a new Property object
+            var property = new Property();
 
-            if (!String.IsNullOrEmpty(address))
-            {
-                properties = properties.Where(p => p.Address.Contains(address));
-            }
+            // Optionally, set some default values for the new property
+            // property.ManagerID = user.UserID;
 
-            return View(properties.ToList());
-        }
-
-
-
-
-
-
-        // GET: Properties/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Property property = db.Properties.Find(id);
-            if (property == null)
-            {
-                return HttpNotFound();
-            }
             return View(property);
         }
 
-        // GET: Properties/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Properties/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PropertyID,ManagerID,Address,Name")] Property property)
+        public ActionResult CreateWithManager([Bind(Include = "PropertyID,ManagerID,Address,Name")] Property property)
         {
             if (ModelState.IsValid)
             {
@@ -76,22 +45,66 @@ namespace PropertyRentalManagementWebSite.Controllers
 
                 db.Properties.Add(property);
                 db.SaveChanges();
+                return RedirectToAction("CreateWithManager");
+            }
+
+            return View(property);
+        }
+        public ActionResult ShowProperties(string address)
+        {
+            var properties = db.Properties.AsQueryable();
+
+            if (!String.IsNullOrEmpty(address))
+            {
+                address = address.Trim().ToLower(); // Convert address to lowercase
+                properties = properties.Where(p => p.Address.ToLower().Contains(address));
+            }
+
+            return View(properties.ToList());
+        }
+
+
+
+
+
+
+
+
+        // GET: Properties
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Properties.ToListAsync());
+        }
+
+        // GET: Properties/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Properties/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "PropertyID,Address,Name,ManagerID")] Property property)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Properties.Add(property);
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
             return View(property);
         }
 
-
-
         // GET: Properties/Edit/5
-        public ActionResult Edit(int? id)
+        public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Property property = db.Properties.Find(id);
+            Property property = await db.Properties.FindAsync(id);
             if (property == null)
             {
                 return HttpNotFound();
@@ -102,25 +115,25 @@ namespace PropertyRentalManagementWebSite.Controllers
         // POST: Properties/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PropertyID,ManagerID,Address,Name")] Property property)
+        public async Task<ActionResult> Edit([Bind(Include = "PropertyID,Address,Name,ManagerID")] Property property)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(property).State = EntityState.Modified;
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(property);
         }
 
         // GET: Properties/Delete/5
-        public ActionResult Delete(int? id)
+        public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Property property = db.Properties.Find(id);
+            Property property = await db.Properties.FindAsync(id);
             if (property == null)
             {
                 return HttpNotFound();
@@ -131,22 +144,13 @@ namespace PropertyRentalManagementWebSite.Controllers
         // POST: Properties/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Property property = db.Properties.Include(p => p.Apartments).SingleOrDefault(p => p.PropertyID == id);
-            if (property != null)
-            {
-                // Remove all related apartments
-                db.Apartments.RemoveRange(property.Apartments);
-
-                // Remove the property
-                db.Properties.Remove(property);
-
-                db.SaveChanges();
-            }
+            Property property = await db.Properties.FindAsync(id);
+            db.Properties.Remove(property);
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
-
 
         protected override void Dispose(bool disposing)
         {
@@ -158,3 +162,4 @@ namespace PropertyRentalManagementWebSite.Controllers
         }
     }
 }
+
--- Models/Property.cs
+++ Models/Property.cs
@@ -11,7 +11,9 @@ namespace PropertyRentalManagementWebSite.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public partial class Property
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,12 +21,19 @@ namespace PropertyRentalManagementWebSite.Models
         {
             this.Apartments = new HashSet<Apartment>();
         }
-    
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PropertyID { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Only letters, numbers, and spaces are allowed.")]
         public string Address { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Only letters and spaces are allowed.")]
         public string Name { get; set; }
+
         public int ManagerID { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Apartment> Apartments { get; set; }
         public virtual User User { get; set; }
